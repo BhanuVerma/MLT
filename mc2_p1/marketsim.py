f77184dfd2d785de2e4eef426299d924f9669f8c
Bhanu Verma <bhanu.iiitd@gmail.com>
2015-10-11 01:44:16 -0400

    changed folder structure and corrected code

--- mc2_p1/marketsim.py
+++ mc2_p1/marketsim.py
@@ -5,7 +5,6 @@ import numpy as np
 import os
 import csv
 
-
 from util import get_data, plot_data
 from portfolio.analysis import get_portfolio_value, get_portfolio_stats, plot_normalized_data
 
@@ -29,37 +28,66 @@ def compute_portvals(start_date, end_date, orders_file, start_val):
     reader = csv.reader(open(orders_file, 'rU'), delimiter=',')
 
     # eliminate duplicate symbols
-    symbol = []
+    symbols = []
     symbol_dict = {}
     i = 0
     for row in reader:
         if i != 0:
             if (symbol_dict.get(row[1], -1)) == -1:
                 symbol_dict[row[1]] = row[1]
-                symbol.append(row[1])
+                symbols.append(row[1])
 
         i += 1
 
-    # create dataframe
+    # create dataframes
     dates = pd.date_range(start_date, end_date)
-    df = pd.DataFrame(index=dates, columns=symbol)
-    df = df.fillna(0)
+    prices_all = get_data(symbols, dates)  # automatically adds SPY
+    prices_df = prices_all[symbols]  # only portfolio symbols
+    prices_df['Cash'] = 1.0
 
-    print df
-    print ""
-    # populate dataframe
+    count_df = pd.DataFrame(index=prices_df.index, columns=symbols)
+    count_df = count_df.fillna(0)
+
+    cash_df = pd.DataFrame(index=prices_df.index, columns=['Cash_Value'])
+    cash_df = cash_df.fillna(start_val)
+
+    # populate dataframes
     reader = csv.reader(open(orders_file, 'rU'), delimiter=',')
     i = 0
     for row in reader:
         if i != 0:
-            if row[0] in df.index:
-                df.ix[row[0], row[1]] = row[3]
+            if row[0] in count_df.index:
+                if row[2] == 'SELL':
+                    count_df.ix[row[0], row[1]] += -float(row[3])
+                    cash_df.ix[row[0]] += -float(row[3]) * prices_df.ix[row[0], row[1]]
+                if row[2] == 'BUY':
+                    count_df.ix[row[0], row[1]] += float(row[3])
+                    cash_df.ix[row[0]] += float(row[3]) * prices_df.ix[row[0], row[1]]
 
         i += 1
 
-    print df
+    value = start_val
+    for date_index, row in count_df.iterrows():
+        for i in range(0, len(row), 1):
+            if date_index in count_df.index and date_index in prices_df.index:
+                value += -(prices_df.ix[date_index, symbols[i]] * count_df.ix[date_index, symbols[i]])
+        cash_df.ix[date_index] = value
 
-    return 0
+    count_df['Cash'] = cash_df
+
+    for i in range(0, len(count_df.columns)-1, 1):
+        count_df[symbols[i]] = count_df[symbols[i]].cumsum()
+
+    count_df = prices_df * count_df
+    count_df['Sum'] = count_df.sum(axis=1)
+
+    # rearrange columns
+    columns = count_df.columns.tolist()
+    columns = columns[-1:] + columns[:-1]
+    count_df = count_df[columns]
+
+    # print count_df['Sum']
+    return count_df
 
 
 def test_run():
@@ -67,40 +95,40 @@ def test_run():
     # Define input parameters
     start_date = '2011-01-05'
     end_date = '2011-01-20'
-    orders_file = os.path.join("orders", "orders.csv")
+    orders_file = os.path.join("orders", "orders-short.csv")
     start_val = 1000000
     # Process orders
     portvals = compute_portvals(start_date, end_date, orders_file, start_val)
     if isinstance(portvals, pd.DataFrame):
         portvals = portvals[portvals.columns[0]]  # if a DataFrame is returned select the first column to get a Series
-    
-    # Get portfolio stats
-    #cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
 
-    # Simulate a $SPX-only reference portfolio to get stats
-    # prices_SPX = get_data(['$SPX'], pd.date_range(start_date, end_date))
-    # prices_SPX = prices_SPX[['$SPX']]  # remove SPY
-    # portvals_SPX = get_portfolio_value(prices_SPX, [1.0])
-    # cum_ret_SPX, avg_daily_ret_SPX, std_daily_ret_SPX, sharpe_ratio_SPX = get_portfolio_stats(portvals_SPX)
-    #
-    # # Compare portfolio against $SPX
-    # print "Data Range: {} to {}".format(start_date, end_date)
-    # print
-    # print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
-    # print "Sharpe Ratio of $SPX: {}".format(sharpe_ratio_SPX)
-    # print
-    # print "Cumulative Return of Fund: {}".format(cum_ret)
-    # print "Cumulative Return of $SPX: {}".format(cum_ret_SPX)
-    # print
-    # print "Standard Deviation of Fund: {}".format(std_daily_ret)
-    # print "Standard Deviation of $SPX: {}".format(std_daily_ret_SPX)
-    # print
-    # print "Average Daily Return of Fund: {}".format(avg_daily_ret)
-    # print "Average Daily Return of $SPX: {}".format(avg_daily_ret_SPX)
-    # print
-    # print "Final Portfolio Value: {}".format(portvals[-1])
-    #
-    # # Plot computed daily portfolio value
+    #Get portfolio stats
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
+
+    #Simulate a $SPX-only reference portfolio to get stats
+    prices_SPX = get_data(['$SPX'], pd.date_range(start_date, end_date))
+    prices_SPX = prices_SPX[['$SPX']]  # remove SPY
+    portvals_SPX = get_portfolio_value(prices_SPX, [1.0])
+    cum_ret_SPX, avg_daily_ret_SPX, std_daily_ret_SPX, sharpe_ratio_SPX = get_portfolio_stats(portvals_SPX)
+
+    # Compare portfolio against $SPX
+    print "Data Range: {} to {}".format(start_date, end_date)
+    print
+    print "Sharpe Ratio of Fund: {}".format(sharpe_ratio)
+    print "Sharpe Ratio of $SPX: {}".format(sharpe_ratio_SPX)
+    print
+    print "Cumulative Return of Fund: {}".format(cum_ret)
+    print "Cumulative Return of $SPX: {}".format(cum_ret_SPX)
+    print
+    print "Standard Deviation of Fund: {}".format(std_daily_ret)
+    print "Standard Deviation of $SPX: {}".format(std_daily_ret_SPX)
+    print
+    print "Average Daily Return of Fund: {}".format(avg_daily_ret)
+    print "Average Daily Return of $SPX: {}".format(avg_daily_ret_SPX)
+    print
+    print "Final Portfolio Value: {}".format(portvals[-1])
+
+    # Plot computed daily portfolio value
     # df_temp = pd.concat([portvals, prices_SPX['$SPX']], keys=['Portfolio', '$SPX'], axis=1)
     # plot_normalized_data(df_temp, title="Daily portfolio value and $SPX")
 
